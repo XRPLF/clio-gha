625ba219309cc44eba6ff58c75e5597ce9be29a8
Michael Legleux <mlegleux@ripple.com>
2022-08-03 11:28:36 -0700

    Change SRC_DIR->CLIO_ROOT

--- build/build_clio.sh
+++ build/build_clio.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export SRC_DIR=$(realpath clio_src)
-echo "[${SRC_DIR}] passed to Clio repo as SRC_DIR"
+export CLIO_ROOT=$(realpath clio_src)
+echo "[${CLIO_ROOT}] passed to Clio repo as CLIO_ROOT"
 
-cmake -S clio_ci -B clio_ci/build -DSRC_DIR=$SRC_DIR
+cmake -S clio_ci -B clio_ci/build -DCLIO_ROOT=$CLIO_ROOT
 cmake --build clio_ci/build --parallel $(nproc) | tee -a clio_build.log
 cp /*.log . # get the boost build logs
 cp ./clio_ci/build/clio-prefix/src/clio-build/clio_tests .
